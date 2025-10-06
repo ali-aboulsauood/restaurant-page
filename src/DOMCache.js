840c47239ad46f70110747adce9696c6e397ca94
ali-aboulsauood <aliaboulsauood@gmail.com>
2025-10-06 22:16:20 +0300

    Rename utility module and Use it for DOM Cache
    1. Rename utility module (`src/utils.js`) to DOM cache module (`src/DOMCache.js`)
    2. Remove utility function for creating new element
    3. Move utility function for clearing main page content element (`div#content`) to entry point module (`index.js`)

---
+++ src/DOMCache.js
@@ -0,0 +1,6 @@
+// This module contains all DOM Node references that are needed by multiple modules
+
+// The main page content element, whose content is changed on tab switching.
+const contentElement = document.querySelector('#content');
+
+export default contentElement;
